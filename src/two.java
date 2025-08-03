083b747f0500e5c7b3e9bfbe60f7bc95dc05b28f
sanehakaur77 <snehaswali9988@gmail.com>
2025-08-03 17:29:01 +0530

    Initial commit for valid mountain array

---
+++ src/two.java
@@ -0,0 +1,31 @@
+import java.util.HashSet;
+
+public class two {
+public  static  void main(String args[]){
+    // removing the lowest set Bit
+          int nums[]={1,2,0};
+
+            HashSet<Integer> set=new HashSet<>();
+            for(int i=0;i<nums.length;i++){
+               if(nums[]){
+
+                }
+            }
+            int Xor1=0;
+            int Xor2=0;
+            for(int i=1;i<=set.size();i++){
+                Xor1^=i;
+            }
+            for(int a:set){
+                Xor2^=a;
+
+
+            }
+           System.out.println( Xor1^Xor2);
+        }
+
+
+}
+
+
+
