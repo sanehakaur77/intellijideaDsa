52022af37d6dd192209a582e73582ed9afb1bd74
sanehakaur77 <snehaswali9988@gmail.com>
2025-08-03 17:28:29 +0530

    Initial commit for valid mountain array

---
+++ src/saneha.java
@@ -0,0 +1,13 @@
+public class saneha {
+public static void main(String args[]){
+   // to check that the number is odd or not
+   int num=12;
+   if((num&1)==1){
+      System.out.println("odd");
+   }
+   else{
+      System.out.println("even");
+   }
+
+}
+}
